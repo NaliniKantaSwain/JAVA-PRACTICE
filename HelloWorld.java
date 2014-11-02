23013fa40b4df7a9165e4aad45cfafc0a2150608
NALINI KANTA SWAIN <nalinikanta@outlook.com>
2014-11-03 00:20:25 +0530

    Create HelloWorld.java
    This is first program of java HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+package com.own.CoreJavaBasicProgram;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("HelloWorld");
+
+	}
+
+}
